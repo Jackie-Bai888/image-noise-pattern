59beccee47fc669afd4f294ae91266cf17b7d161
corruption <39644415+realcorruption@users.noreply.github.com>
2022-09-02 19:37:05 +0800

    Update colorCorrection.py

--- colorCorrection.py
+++ colorCorrection.py
@@ -4,22 +4,13 @@ import random
 
 
 def color_correction_error(img):
-    """
-           #颜色矫正异常
-           输入:
-               image     图像
-           返回颜色矫正异常后的图像
-    """
     fImg = img.astype(np.float32)
     fImg = fImg / 255.0
-    # 颜色空间转换 BGR转为HLS
+    #
     hlsImg = cv2.cvtColor(fImg, cv2.COLOR_BGR2HLS)
-    # 随机生成饱和度值
     s = int(random.uniform(-30, 100))
     MAX_VALUE = 100
-    # 调整饱和度和亮度后的效果
     lsImg = np.zeros(img.shape, np.float32)
-    # 调整饱和度 将hlsImg[:, :, 2]中大于1的全部截取
     hlsImg[:, :, 2] = (1.0 + s / float(MAX_VALUE)) * hlsImg[:, :, 2]
     hlsImg[:, :, 2][hlsImg[:, :, 2] > 1] = 1
     # hlsImg[:, :, 2] -= s
@@ -31,7 +22,7 @@ def color_correction_error(img):
 
 
 # '''
-#下面几行代码用来验证增强结果
+#test
 src = cv2.imread("../VOCdevkit/VOC2007/JPEGImages/000000.jpg")
 cv2.imshow('original img', src)
 black_src = color_correction_error(src)
