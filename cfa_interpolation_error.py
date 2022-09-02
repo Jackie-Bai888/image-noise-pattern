1efd415421671a260ae8e07ebf7c14d08ed705b8
corruption <39644415+realcorruption@users.noreply.github.com>
2022-09-02 19:34:04 +0800

    Update cfa_interpolation_error.py

--- cfa_interpolation_error.py
+++ cfa_interpolation_error.py
@@ -2,13 +2,6 @@ import numpy as np
 import cv2
 import random
 def cfa_interpolation_error(img, num_precent=0.6):
-    """
-     #CFA插值异常
-     输入:
-         img     图像
-     返回CFA异常后的图像
-     """
-    # 获取图像长宽通道数
     height, width, _ = img.shape
     num = int(height * width * num_precent)
     pixel_position = random.sample(range(0, height * width), num)
@@ -19,7 +12,7 @@ def cfa_interpolation_error(img, num_precent=0.6):
 
 
 # '''
-#下面几行代码用来验证增强结果
+#test
 src = cv2.imread("../VOCdevkit/VOC2007/JPEGImages/000000.jpg")
 raw_image = cfa_interpolation_error(src, 1)
 cv2.imshow('raw image4', raw_image)
