02c2645d3ed38dfe067f43a70752c68e14eee40c
corruption <39644415+realcorruption@users.noreply.github.com>
2022-09-02 19:30:33 +0800

    Update and rename blackLevel.py to I-BLC_E-BLC.py

--- I-BLC_E-BLC.py
+++ I-BLC_E-BLC.py
@@ -1,6 +1,3 @@
-'''
-黑电平矫正异常：1.将RGB图像转换为RAW单通道图像；2.改变图像的G、R通道，使得图像偏绿或者偏红
-'''
 import random
 
 import cv2
@@ -8,23 +5,21 @@ import numpy as np
 from RGB2RAW import RGB2Bayer_RG
 
 def black_level_correct_more(img, color_num=255):
-    '''模拟黑电平校正过多，图像变绿'''
-    # 生成蒙版，改变图像整体颜色
+    #E-BLC
     mask = np.zeros(img.shape, img.dtype)
     mask[:, :, 1] = color_num
     dst = cv2.addWeighted(img, 0.7, mask, 0.3, 0)
     return dst
 
 def black_level_correct_less(img, color_num=255):
-    '''模拟黑电平校正较少，图像变红'''
-    # 生成蒙版，改变图像整体颜色
+    #I-BLC
     mask = np.zeros(img.shape, img.dtype)
     mask[:, :, 2] = color_num
     dst = cv2.addWeighted(img, 0.7, mask, 0.3, 0)
     return dst
 
 # '''
-#下面几行代码用来验证增强结果
+#test
 ori_img = cv2.imread("../VOCdevkit/VOC2007/JPEGImages/000000.jpg")
 # cv2.imshow('original img', ori_img)
 black_src = black_level_correct_less(ori_img)
