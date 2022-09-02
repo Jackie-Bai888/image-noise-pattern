fc7893d672b2cd58fb92f4ecfd8f5e1c8e275924
corruption <39644415+realcorruption@users.noreply.github.com>
2022-09-02 19:47:42 +0800

    Update and rename loseFocus.py to FM-D.py

--- FM-D.py
+++ FM-D.py
@@ -2,15 +2,6 @@ import cv2
 import random
 
 def lose_focus(img, level=10):
-    """
-    #生成失焦图像,这里使用高斯模糊来模拟
-    输入:
-        img 图像
-        mold 模糊类型
-        level 模糊等级
-    返回：
-        模糊后的图像
-    """
     mold = 1#random.randint(0,1)
     if mold == 0:
         # 均值模糊
@@ -23,7 +14,6 @@ def lose_focus(img, level=10):
 
 
 # '''
-#下面几行代码用来验证增强结果
 src = cv2.imread("../VOCdevkit/VOC2007/JPEGImages/000000.jpg")
 # cv2.imshow('original img', src)
 error_img = lose_focus(src)
